22cd3287e06e05891eebc2247c27575cd4f85e55
Ed Ball <ejball@gmail.com>
2025-07-18 16:23:50 -0700

    Optimize common isolation levels.

--- src/MuchAdo/DbTransactionSettings.cs
+++ src/MuchAdo/DbTransactionSettings.cs
@@ -13,9 +13,37 @@ public class DbTransactionSettings
 	public static DbTransactionSettings Default { get; } = new(null);
 
 	/// <summary>
+	/// Transaction settings with <c>IsolationLevel.ReadUncommitted</c>.
+	/// </summary>
+	public static DbTransactionSettings ReadUncommitted { get; } = new(System.Data.IsolationLevel.ReadUncommitted);
+
+	/// <summary>
+	/// Transaction settings with <c>IsolationLevel.ReadCommitted</c>.
+	/// </summary>
+	public static DbTransactionSettings ReadCommitted { get; } = new(System.Data.IsolationLevel.ReadCommitted);
+
+	/// <summary>
+	/// Transaction settings with <c>IsolationLevel.RepeatableRead</c>.
+	/// </summary>
+	public static DbTransactionSettings RepeatableRead { get; } = new(System.Data.IsolationLevel.RepeatableRead);
+
+	/// <summary>
+	/// Transaction settings with <c>IsolationLevel.Serializable</c>.
+	/// </summary>
+	public static DbTransactionSettings Serializable { get; } = new(System.Data.IsolationLevel.Serializable);
+
+	/// <summary>
 	/// Creates transaction settings with the specified isolation level.
 	/// </summary>
-	public static DbTransactionSettings FromIsolationLevel(IsolationLevel isolationLevel) => new(isolationLevel);
+	public static DbTransactionSettings FromIsolationLevel(IsolationLevel isolationLevel) =>
+		isolationLevel switch
+		{
+			System.Data.IsolationLevel.ReadUncommitted => ReadUncommitted,
+			System.Data.IsolationLevel.ReadCommitted => ReadCommitted,
+			System.Data.IsolationLevel.RepeatableRead => RepeatableRead,
+			System.Data.IsolationLevel.Serializable => Serializable,
+			_ => new DbTransactionSettings(isolationLevel),
+		};
 
 	/// <summary>
 	/// Implicitly casts an isolation level to transaction settings with that isolation level.
