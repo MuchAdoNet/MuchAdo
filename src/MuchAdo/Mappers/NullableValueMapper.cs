819e2a4fdb6b9bde9cbc353c75c298f007f4bd2c
Ed Ball <ed.ball@logos.com>
2026-05-04 13:43:03 -0700

    Fix nullable multi-field null checks (#16)

--- src/MuchAdo/Mappers/NullableValueMapper.cs
+++ src/MuchAdo/Mappers/NullableValueMapper.cs
@@ -8,5 +8,5 @@ internal sealed class NullableValueMapper<T>(DbTypeMapper<T> mapper) : DbTypeMap
 	public override int? FieldCount => mapper.FieldCount;
 
 	protected override T? MapCore(IDataRecord record, int index, int count, DbConnectorRecordState? state) =>
-		!record.IsDBNull(index) ? mapper.Map(record, index, count, state) : null;
+		!Utility.IsAllNull(record, index, count) ? mapper.Map(record, index, count, state) : null;
 }
